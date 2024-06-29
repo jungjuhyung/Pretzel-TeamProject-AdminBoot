2b4248a227b32db953c0a260b547c7a74aac2dfb
wngud <ramyun1537@gmail.com>
2024-06-29 20:57:26 +0900

    0629주형

--- src/main/java/com/ict/pretzel_admin/jung/controller/MovieController.java
+++ src/main/java/com/ict/pretzel_admin/jung/controller/MovieController.java
@@ -16,6 +16,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/movie")
 public class MovieController {
     
+	@GetMapping("/search")
+	public String search(@RequestParam("query") String query, @RequestParam("year") String year) {
+		try {
+			String apiURL = "";
+			if (year.equals("")) {
+				apiURL = "https://api.themoviedb.org/3/search/movie?query="+query+"&include_adult=true&language=ko-kr&language=en-US";
+			}else{
+				apiURL = "https://api.themoviedb.org/3/search/movie?query="+query+"&year="+year+"&include_adult=true&language=ko-kr&language=en-US";
+			}
+			URL url = new URL(apiURL);
+			String api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmE1OThkMzg4OTgwZjBlMTJjNmU1N2RkYjRmNjFlNyIsInN1YiI6IjY2NzEzMGNlNDA1YjNhMjk3MDZhYWFlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cT8hOciOWfO-qUWSh_fzqQzVburxqSAqwdXoaTgHz1E";
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			
+			conn.setRequestMethod("GET");
+			
+			conn.setRequestProperty("accept", "application/json");
+			conn.setRequestProperty("Authorization", "Bearer "+api_key);
+			int responeseCode = conn.getResponseCode();
+			System.out.println(responeseCode);
+			if(responeseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br =
+						new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+				
+				String line ="";
+				StringBuffer sb = new StringBuffer();
+				while((line=br.readLine()) !=null) {
+					sb.append(line);
+				}
+				String result = sb.toString();
+				System.out.println(result);
+				return result;
+			}
+			
+		} catch (Exception e) {
+			System.out.println("연결 실패");
+		}
+		return null;
+	}
+
 	@GetMapping("/detail")
 	public String detail(@RequestParam("movie_id") String movie_id) {
 		try {
