3ad19a1d17cfae63a188811bf16018ed73eb08ab
wngud <ramyun1537@gmail.com>
2024-07-02 15:27:22 +0900

    최종 관리자

--- src/main/java/com/ict/pretzel_admin/vo/MovieVO.java
+++ src/main/java/com/ict/pretzel_admin/vo/MovieVO.java
@@ -8,7 +8,8 @@ import lombok.Data;
 public class MovieVO {
     private String movie_idx, movie_id, tmdb_title, korea_title, english_title, thema,
     synopsis, movie_url, subtitle_url,trailer_url,poster_url,backdrop_url,movie_grade,runtime,
-    release_date,status,insert_time,update_time,delete_time,admin_id,watch_stack,synchro,movie_del_name,sub_del_name;
+    release_date,status,insert_time,update_time,delete_time,admin_id,watch_stack,synchro,movie_del_name,sub_del_name,
+    teen_stack,twenty_stack,thirty_stack,forty_stack,fifty_stack,male_stack,female_stack;
     private MultipartFile movie;
     private MultipartFile subtitle;
 }
