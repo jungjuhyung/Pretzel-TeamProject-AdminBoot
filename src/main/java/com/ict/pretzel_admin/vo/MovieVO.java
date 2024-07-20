8159d3c993805d6357b66a68db34e6cf9c94a240
wngud <ramyun1537@gmail.com>
2024-07-20 23:41:51 +0900

    movieVO ai동기화 추가

--- src/main/java/com/ict/pretzel_admin/vo/MovieVO.java
+++ src/main/java/com/ict/pretzel_admin/vo/MovieVO.java
@@ -8,6 +8,7 @@ import lombok.Data;
 public class MovieVO {
     private String movie_idx, movie_id, tmdb_title, korea_title, english_title, thema,
     synopsis, movie_url, storage_name,trailer_url,poster_url,backdrop_url,movie_grade,runtime,
+    ai_deep_syc,ai_emotion_syc,
     release_date,status,insert_time,update_time,delete_time,admin_id,watch_stack,synchro,
     teen_stack,twenty_stack,thirty_stack,forty_stack,fifty_stack,male_stack,female_stack;
     private MultipartFile movie;
